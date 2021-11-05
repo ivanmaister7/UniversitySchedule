4058c86c3b37cbb566cbec7d277154c8ac9a6ce4
Iliianotapoet <ilya.10189@gmail.com>
2021-11-05 09:07:10 +0200

    temp:

--- src/main/java/com/schedule/proj/exсeption/NotFoundExceptionHandler.java
+++ src/main/java/com/schedule/proj/exсeption/NotFoundExceptionHandler.java
@@ -3,14 +3,15 @@ package com.schedule.proj.exсeption;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class NotFoundExceptionHandler {
-    @ExceptionHandler({ StudentNotFoundException.class, TeacherNotFoundException.class })
+    @ExceptionHandler({ StudentNotFoundException.class })
     public ResponseEntity<Object> handleStudentNotFound(
             Exception ex, WebRequest request) {
-        String[] pathSplited = request.getContextPath().split("/");
+        String[] pathSplited = ((ServletWebRequest)request).getRequest().getRequestURI().split("/");
         return ResponseEntity.badRequest().body(
                 "Student not found, id: " + pathSplited[3]);
     }
@@ -18,7 +19,7 @@ public class NotFoundExceptionHandler {
     @ExceptionHandler({ TeacherNotFoundException.class })
     public ResponseEntity<Object> handleTeacherNotFound(
             Exception ex, WebRequest request) {
-        String[] pathSplited = request.getContextPath().split("/");
+        String[] pathSplited = ((ServletWebRequest)request).getRequest().getRequestURI().split("/");
         return ResponseEntity.badRequest().body(
                 "Teacher not found, id: " + pathSplited[3]);
     }
@@ -26,7 +27,7 @@ public class NotFoundExceptionHandler {
     @ExceptionHandler({ SubjectNotFoundException.class })
     public ResponseEntity<Object> handleSubjectNotFound(
             Exception ex, WebRequest request) {
-        String[] pathSplited = request.getContextPath().split("/");
+        String[] pathSplited = ((ServletWebRequest)request).getRequest().getRequestURI().split("/");
         return ResponseEntity.badRequest().body(
                 "Subject not found: " + pathSplited[3]);
     }
