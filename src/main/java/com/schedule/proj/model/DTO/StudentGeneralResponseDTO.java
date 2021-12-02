e741a480e767a48fb465e1d1fdfc17bd40f42004
alehandromaniuk <57686519+alehandromaniuk@users.noreply.github.com>
2021-12-02 20:18:39 +0200

    update

---
+++ src/main/java/com/schedule/proj/model/DTO/StudentGeneralResponseDTO.java
@@ -0,0 +1,66 @@
+package com.schedule.proj.model.DTO;
+
+import lombok.AllArgsConstructor;
+
+import javax.persistence.Column;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+@AllArgsConstructor
+public class StudentGeneralResponseDTO {
+
+
+
+
+    private  String firstname;
+
+    private  String lastname;
+
+    private String faculty;
+
+    private String speciality;
+
+    private Integer studentYear;
+
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public Integer getStudentYear() {
+        return studentYear;
+    }
+
+    public void setStudentYear(Integer studentYear) {
+        this.studentYear = studentYear;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
