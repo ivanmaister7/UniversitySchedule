74c6e32191cdda3ffd6f40fef408e25417564a3e
alehandromaniuk <57686519+alehandromaniuk@users.noreply.github.com>
2021-12-09 19:41:58 +0200

    1

--- src/main/java/com/schedule/proj/controller/UserController.java
+++ src/main/java/com/schedule/proj/controller/UserController.java
@@ -1,16 +1,22 @@
 package com.schedule.proj.controller;
 
 
+import com.schedule.proj.exсeption.RegistrationException;
+import com.schedule.proj.model.DTO.PasswordDTO;
 import com.schedule.proj.model.User;
 import com.schedule.proj.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Controller
@@ -30,7 +36,21 @@ public class UserController {
     }
 
 
+    @Operation(summary = "change password")
+    @PutMapping("/changePassword")
+    ResponseEntity<?> changePassword(@RequestBody PasswordDTO dto,
+                                     HttpServletRequest request) {
 
+        Map<String, String> res = new HashMap<>();
+        try {
+            String result = userService.changePassword(request, dto.getOldPassword(), dto.getNewPassword());
+            res.put("message", result);
+            return ResponseEntity.ok(res);
+        } catch (RegistrationException | UsernameNotFoundException e) {
+            res.put("message", e.getMessage());
+            return ResponseEntity.badRequest().body(res);
+        }
+    }
 
 
 
--- src/main/java/com/schedule/proj/service/UserService.java
+++ src/main/java/com/schedule/proj/service/UserService.java
@@ -1,17 +1,22 @@
 package com.schedule.proj.service;
 
+import com.schedule.proj.exсeption.RegistrationException;
 import com.schedule.proj.exсeption.StudentNotFoundException;
 import com.schedule.proj.model.DTO.StudentResponseDTO;
 import com.schedule.proj.model.User;
 import com.schedule.proj.repository.UserRepository;
+import com.schedule.proj.security.jwt.JwtProvider;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,6 +31,8 @@ public class UserService {
     UserRepository userRepository;
     @Autowired
     EmailService emailService;
+    PasswordService passwordService;
+    JwtProvider jwtProvider;
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
@@ -75,6 +82,25 @@ public class UserService {
 
     public  User findUserByEmail(String email){
        return userRepository.findUserByEmail(email);
+    }
+
+    public String changePassword(HttpServletRequest request, String oldPassword, String newPassword) {
+        String token = jwtProvider.getTokenFromRequest(request);
+        String email =jwtProvider.getLoginFromToken(token);
+        User user = userRepository.findUserByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found ");
+        }
+        if(passwordService.equalsPassword(oldPassword, user.getPassword())){
+            if(!passwordService.isValidPassword(newPassword)){
+                throw new RegistrationException("Password is not valid");
+            }
+            user.setPassword(passwordService.encodePassword(newPassword));
+            userRepository.save(user);
+            return "Password changed";
+        }else{
+            throw new RegistrationException("The old password is incorrect");
+        }
     }
 
 }
