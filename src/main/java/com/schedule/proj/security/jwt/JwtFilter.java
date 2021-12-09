409cdc103e11f73f31ec57ca000348be1cbb8723
alehandromaniuk <57686519+alehandromaniuk@users.noreply.github.com>
2021-12-09 17:05:07 +0200

    1

--- src/main/java/com/schedule/proj/security/jwt/JwtFilter.java
+++ src/main/java/com/schedule/proj/security/jwt/JwtFilter.java
@@ -17,7 +17,8 @@ import java.io.IOException;
 
 
 @Component
-public class JwtFilter extends GenericFilterBean {
+public class
+JwtFilter extends GenericFilterBean {
 
     @Autowired
     private JwtProvider jwtProvider;
@@ -39,7 +40,7 @@ public class JwtFilter extends GenericFilterBean {
             System.out.println(e.getMessage());
         }
 
-        filterChain.doFilter(servletRequest, servletResponse);
+        // filterChain.doFilter(servletRequest, servletResponse);
     }
 
 
